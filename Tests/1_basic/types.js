5dee12a8ea41c233c964aedb431e78dfcf37e858
Ruslan Turalinov <rusltur_24@mail.ru>
2024-10-26 13:47:26 +0500

    added types.js

---
+++ Tests/1_basic/types.js
@@ -0,0 +1,10 @@
+const storeName = 'Sabina';
+const storeDescription = {
+  budget: 10000,
+  employees: ['Alina', 'Sabina', 'Sofiya'],
+  products: {
+    kinder: 100,
+    milk: 10
+  },
+  open: true
+};
